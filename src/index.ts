ac5d90ef09cb9ea390ddcc31cae28d511863ab85
Albert Chang <a89529294@hotmail.com>
2025-03-27 14:41:03 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+
+const app = new Hono();
+app.use("*", cors());
+
+app.get("/", (c) => {
+  return c.json({ success: true });
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  }
+);
