605e82266a5df87e810825e063dd93a0362ba614
Albert Chang <a89529294@hotmail.com>
2025-03-31 13:23:25 +0800

    add seed script

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -5,13 +5,20 @@ import { usersTable } from "./schema.js";
 import { db } from "./index.js";
 
 async function main() {
-  const user: typeof usersTable.$inferInsert = {
+  await db.delete(usersTable);
+
+  const user1: typeof usersTable.$inferInsert = {
     name: "John",
     age: 30,
     email: "john@example.com",
   };
+  const user2: typeof usersTable.$inferInsert = {
+    name: "Amy",
+    age: 30,
+    email: "amy@example.com",
+  };
 
-  await db.insert(usersTable).values(user);
+  await db.insert(usersTable).values([user1, user2]);
   console.log("New user created!");
 
   /*
